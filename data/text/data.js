6053ff51f53e23c50014e61b0f90caa4343bf7bc
arkana <arkana.faisal.r@gmail.com>
2025-08-11 22:00:11 +0700

    Create data.js

---
+++ data/text/data.js
@@ -0,0 +1,16 @@
+const itemData = [
+    {
+        name: 't-shirt',
+        price: ["80k", "80k", "80k", "80k", "85k", "85k"],
+        image: "./data/images/t-shirt-front.png",
+        bonus: ["sticker"],
+        size: ["S", "M", "L", "XL", "XXL"],
+        panjang: ["68", "70", "73", "74", "75"],
+        lebar: ["44", "47", "50", "53", "56"]
+    },
+    {
+        name: 'scarf',
+        price: ["80k"],
+        image: "./data/images/scarf.png",
+    },
+]
